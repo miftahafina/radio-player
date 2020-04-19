31a9d119f4e765b38b470e30d643d12aa654d4bb
Miftah Afina <miftah.afina@gmail.com>
2020-04-19 15:04:58 +0700

    Toggle play using react-player

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactPlayer from 'react-player';
+
 import 'normalize.css';
 import './App.scss';
+
 import PlayIcon from './play.png';
 import PauseIcon from './pause.png';
 
 function App() {
+  const [playing, setPlaying] = useState(true);
+  const [icon, setIcon] = useState(PauseIcon);
+
+  const handleClick = () => {
+    playing === true ? setPlaying(false) : setPlaying(true);
+  }
+
+  useEffect(() => {
+    console.log('playing:', playing);
+    playing === false ? setIcon(PlayIcon) : setIcon(PauseIcon);
+  }, [playing])
+
   return (
     <div className="container">
       <div className="artwork"></div>
@@ -15,9 +30,11 @@ function App() {
         <p className="freq">107.9 FM</p>
       </div>
 
+      <ReactPlayer url="https://pu.klikhost.com:7204/stream" playing={playing} className="audio"/>
+
       <div className="control">
-        <button className="play">
-          <img src={PlayIcon} alt="play button" />
+        <button className="play" onClick={handleClick}>
+          <img src={icon} alt="play button"/>
         </button>
       </div>
     </div>
